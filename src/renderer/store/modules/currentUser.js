f26a6a16482bcb9aaa861fe501eaf941a6c1e6ad
Wenish <jonas.voland@bluewin.ch>
2018-03-06 19:41:09 +0100

    added currentuser store module

---
+++ src/renderer/store/modules/currentUser.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actionTypes'
+import * as getterNames from '../getterNames'
+import * as mutationTypes from '../mutationTypes'
+
+const state = {
+  displayName: '',
+  email: '',
+  uid: null,
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
